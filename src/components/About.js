8991b0f17735cb4ac14599a4d190aa0e6a8d92f1
Roymaniac <amolojoseph2020@gmail.com>
2022-08-05 16:45:45 +0100

    started about me design

--- src/components/About.js
+++ src/components/About.js
@@ -24,6 +24,27 @@ export default function About() {
               <br /> Designer
             </span>
           </h2>
+
+          <p id="about-text">
+            Ceejay is a UI/UX Designer, a content writer and a front-end
+            developer with a trending view, high-quality and Professional
+            approach and more than 1year. of deep experience working across
+            UX/UI, creating UI for Websites and Web apps. My years of experience
+            include Website design, landing pages, E-commerce websites and
+            online stores, Mobile versions of websites(responsive),
+            Presentations, banners, Email Templates, Marketing materials, UI
+            Kit, Wireframming and Prototyping, UI/UX for mobile (ios & android)
+            and Web apps, Logo design and Branding.
+          </p>
+
+          <p id="about-text">
+            I love working on UI/UX analysis and tasks of complexity never
+            frightened me. All images, fonts and layers will be organized and
+            prepared for the font-end developer.I have an up-to-date knowledge
+            of Web-design industry including Adobe (photoshop, illustrator, XD),
+            Figma, Marvel, Miro , Sketch, Invision App. I have knowledge related
+            to this ground, such as Html, CSS, Javascript , React and C#.
+          </p>
         </div>
         <div className="flex-2">
           <img src={aboutPic} alt="" width={500} />
